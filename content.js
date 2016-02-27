706ff78d6a4278084cac46ad997b83d936260bd7
Heng-Yi Wu <henry40408@gmail.com>
2016-02-28 01:41:02 +0800

    multiple tasks
    - decorate stars block
    - focus on interesting elements

--- content.js
+++ content.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-    var IS_AWESOME = document.title.match(/awesome/);
-
-    var $readme_a = $("#readme a");
-
-    if (IS_AWESOME) {
-        chrome.runtime.sendMessage({
-            type: GET_ACCESS_TOKEN,
-            count: $readme_a.length.toString()
-        }, function(response) {
-            // empty
-        });
-    }
+    document.title.match(/\/awesome-/) && initialize();
 });
+
+function initialize() {
+    var $filtered_readme_a = $("#readme a").filter(function() {
+        return !!$(this).attr('href').match(GITHUB_LINK_PATTERN);
+    });
+
+    chrome.runtime.sendMessage({
+        type: GET_ACCESS_TOKEN,
+        count: $filtered_readme_a.length.toString()
+    }, function(response) {
+        $filtered_readme_a.each(function() {
+            $(this).after($("<span>")
+                .addClass("awesome-stars")
+                .append("\u2605 0"));
+        });
+    });
+}
