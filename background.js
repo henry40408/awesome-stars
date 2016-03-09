9d6557f9762e16496a41c5fd178dea0e19530113
Heng-Yi Wu <henry40408@gmail.com>
2016-03-09 10:24:37 +0800

    add "fancy stars" switch

--- background.js
+++ background.js
@@ -4,9 +4,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     switch (request.type) {
         case GET_OPTION:
             chrome.storage.sync.get("awesome_stars_options", function(value) {
-                value.awesome_stars_options = value.awesome_stars_options || {};
+                var awesome_stars_options = value.awesome_stars_options || {};
                 sendResponse({
-                    access_token: value.awesome_stars_options.access_token || ""
+                    access_token: awesome_stars_options.access_token || "",
+                    fancy_stars: awesome_stars_options.fancy_stars,
                 });
             });
             break;
@@ -14,6 +15,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             chrome.storage.sync.set({
                 awesome_stars_options: {
                     access_token: request.access_token,
+                    fancy_stars: request.fancy_stars,
                 },
             }, function() {
                 sendResponse({
--- options.js
+++ options.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     var $rate_limit = $("#rate-limit"),
         $options_form = $("#options-form"),
         $access_token_field = $("#access-token"),
+        $fancy_stars_field = $("#fancy-stars"),
         $options_form_state = $("#options-form-state");
 
     function update_rate_limit() {
@@ -24,13 +25,18 @@ $(document).ready(function() {
     chrome.runtime.sendMessage({
         type: GET_OPTION
     }, function(response) {
+        if (typeof response.fancy_stars === "undefined")
+            response.fancy_stars = true;
+
         $access_token_field.val(response.access_token);
+        $fancy_stars_field.attr("checked", response.fancy_stars);
     });
 
     $options_form.submit(function(e) {
         chrome.runtime.sendMessage({
             type: SET_OPTION,
-            access_token: $access_token_field.val()
+            access_token: $access_token_field.val(),
+            fancy_stars: $fancy_stars_field.is(":checked"),
         }, function(response) {
             $options_form_state.text("Saved!");
             setTimeout(function() {
