c5e61cdbbcedc57f0c96c9194b93b31b319f1a52
Heng-Yi Wu <henry40408@gmail.com>
2017-03-19 00:26:22 +0800

    Remove first release

--- utils.js
+++
@@ -1,6 +0,0 @@
-var GET_OPTION = "GET_OPTION";
-var SET_OPTION = "SET_OPTION";
-var GET_CACHE_ACCESS_TOKEN = "GET_CACHE_ACCESS_TOKEN";
-var SET_CACHE = "SET_CACHE";
-
-var GITHUB_LINK_PATTERN = /^https?:\/\/github.com\/([A-Za-z0-9-]+?)\/([A-Za-z0-9-]+?)\/?$/;
