ab9e294b0fa902bda6a3ee42faf9ba3d3302adca
Heng-Yi Wu <henry40408@gmail.com>
2017-03-19 00:44:25 +0800

    Regenerate project with Yeoman generator

---
+++ app/scripts/background.js
@@ -0,0 +1,10 @@
+// Enable chromereload by uncommenting this line:
+// import 'chromereload/devonly';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.browserAction.setBadgeText({text: '\'Allo'});
+
+console.log('\'Allo \'Allo! Event Page for Browser Action');
---
+++ tasks/scripts.js
@@ -0,0 +1,59 @@
+import gulp from 'gulp';
+import gulpif from 'gulp-if';
+import { log, colors} from 'gulp-util';
+import named from 'vinyl-named';
+import webpack from 'webpack';
+import gulpWebpack from 'webpack-stream';
+import plumber from 'gulp-plumber';
+import livereload from 'gulp-livereload';
+import args from './lib/args';
+
+const ENV = args.production ? 'production' : 'development';
+
+gulp.task('scripts', (cb) => {
+  return gulp.src('app/scripts/*.js')
+    .pipe(plumber({
+      errorHandler: function() {
+        // Webpack will log the errors
+      }
+    }))
+    .pipe(named())
+    .pipe(gulpWebpack({
+      devtool: args.sourcemaps ? 'inline-source-map': null,
+      watch: args.watch,
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {
+            'NODE_ENV': JSON.stringify(ENV)
+          },
+          '__ENV__': JSON.stringify(ENV),
+          '__VENDOR__': JSON.stringify(args.vendor)
+        }),
+      ].concat(args.production ? [
+        new webpack.optimize.UglifyJsPlugin()
+      ] : []),
+      module: {
+        preLoaders: [{
+          test: /\.js$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/
+        }],
+        loaders: [{
+          test: /\.js$/,
+          loader: 'babel'
+        }]
+      },
+      eslint: {
+        configFile: '.eslintrc'
+      }
+    }, null, (err, stats) => {
+      log(`Finished '${colors.cyan('scripts')}'`, stats.toString({
+        chunks: false,
+        colors: true,
+        cached: false,
+        children: false
+      }));
+    }))
+    .pipe(gulp.dest(`dist/${args.vendor}/scripts`))
+    .pipe(gulpif(args.watch, livereload()));
+});
