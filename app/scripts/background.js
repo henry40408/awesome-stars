cf8663b2579bbe4e5606f57b50aa9628a64f2ea7
Heng-Yi Wu <henry40408@gmail.com>
2017-05-23 20:21:50 +0800

    chore: introduce Airbnb eslint configuration

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,10 +1,6 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
-});
+chrome.browserAction.setBadgeText({ text: '\'Allo' });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+// console.log('\'Allo \'Allo! Event Page for Browser Action');
