0dc891257462deb414d44b06e733041d34706c90
Heng-Yi Wu <henry40408@gmail.com>
2017-05-26 21:29:10 +0800

    feat: add background image to options page

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,1 +1,11 @@
-// console.log('\'Allo \'Allo! Option');
+import jQuery from 'jquery';
+
+const Images = {
+  OPTIONS_BACKGROUND: chrome.extension.getURL('images/options-background.svg'),
+};
+
+jQuery(document).ready(() => {
+  jQuery('body').css({
+    'background-image': `url(${Images.OPTIONS_BACKGROUND})`,
+  });
+});
