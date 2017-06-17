67b8fb77340b11c2d4d63120b02b8185c9a1dd0a
Heng-Yi Wu <henry40408@gmail.com>
2017-06-18 05:37:26 +0800

    refactor(scripts): merge files to make gulp-watch happy

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -8,19 +8,25 @@ import { TextColor } from './constants';
 
 // Constants //
 
-const COLORS = { BLUE: 'blue', ORANGE: 'orange', WHITE: 'white', YELLOW: 'yellow' };
+const COLORS = {
+  BLUE: 'blue',
+  ORANGE: 'orange',
+  WHITE: 'white',
+  YELLOW: 'yellow',
+};
+
 const STYLES = {
-    STAR: {
-        'background-color': 'transparent',
-        margin: '0 .25rem 0 0',
-    },
-    TAG: {
-        'background-color': '#3F3F3F',
-        'border-radius': '.78125rem',
-        'font-size': '.75rem',
-        margin: '0 0 0 .25rem',
-        padding: '.3125rem .625rem .3125rem .4375rem',
-    },
+  STAR: {
+    'background-color': 'transparent',
+    margin: '0 .25rem 0 0',
+  },
+  TAG: {
+    'background-color': '#3F3F3F',
+    'border-radius': '.78125rem',
+    'font-size': '.75rem',
+    margin: '0 0 0 .25rem',
+    padding: '.3125rem .625rem .3125rem .4375rem',
+  },
 };
 
 // Semi-global Variables //
@@ -30,70 +36,70 @@ const client = new Client(chrome.runtime);
 // Local Functions //
 
 function starFromColor(rawColor) {
-    const availableColors = lodash.values(COLORS);
-    const color = lodash.includes(availableColors, rawColor) ? rawColor : COLORS.BLUE;
-    return chrome.extension.getURL(`images/star-${color}.svg`);
+  const availableColors = lodash.values(COLORS);
+  const color = lodash.includes(availableColors, rawColor) ? rawColor : COLORS.BLUE;
+  return chrome.extension.getURL(`images/star-${color}.svg`);
 }
 
 function colorsFromStarCount(starCount) {
-    switch (true) {
-        case (starCount >= 10000):
-            return { star: COLORS.ORANGE, text: TextColor.ORANGE };
-        case (starCount < 10000 && starCount >= 5000):
-            return { star: COLORS.YELLOW, text: TextColor.YELLOW };
-        case (starCount < 5000 && starCount >= 1000):
-            return { star: COLORS.WHITE, text: TextColor.WHITE };
-        default:
-            return { star: COLORS.BLUE, text: TextColor.BLUE };
-    }
+  switch (true) {
+    case (starCount >= 10000):
+      return { star: COLORS.ORANGE, text: TextColor.ORANGE };
+    case (starCount < 10000 && starCount >= 5000):
+      return { star: COLORS.YELLOW, text: TextColor.YELLOW };
+    case (starCount < 5000 && starCount >= 1000):
+      return { star: COLORS.WHITE, text: TextColor.WHITE };
+    default:
+      return { star: COLORS.BLUE, text: TextColor.BLUE };
+  }
 }
 
 async function appendStarTagAsync(el, owner, name) {
-    const $count = jQuery('<span>').text('...');
+  const $count = jQuery('<span>').text('...');
 
-    const $star = jQuery('<img>').css(STYLES.STAR)
-        .attr('src', starFromColor(COLORS.WHITE));
+  const $star = jQuery('<img>').css(STYLES.STAR)
+    .attr('src', starFromColor(COLORS.WHITE));
 
-    const $tag = jQuery('<span>').css({ ...STYLES.TAG, color: TextColor.WHITE })
-        .append($star).append($count);
+  const $tag = jQuery('<span>').css({ ...STYLES.TAG, color: TextColor.WHITE })
+    .append($star).append($count);
 
-    jQuery(el).after($tag);
+  jQuery(el).after($tag);
 
-    const { data: starCount } = await client.message('/stars/get', { owner, name });
-    const formattedStarCount = starCount > 0 ? numeral(starCount).format('0,0') : 'N/A';
+  const { data: starCount } = await client.message('/stars/get', { owner, name });
+  const formattedStarCount = starCount > 0 ? numeral(starCount).format('0,0') : 'N/A';
 
-    $star.css(STYLES.STAR).attr('src', starFromColor(colorsFromStarCount(starCount).star));
-    $tag.css({ ...STYLES.TAG, color: colorsFromStarCount(starCount).text });
-    $count[0].innerHTML = formattedStarCount;
+  $star.css(STYLES.STAR).attr('src', starFromColor(colorsFromStarCount(starCount).star));
+  $tag.css({ ...STYLES.TAG, color: colorsFromStarCount(starCount).text });
+  $count[0].innerHTML = formattedStarCount;
 }
 
 async function iterateAllLinks() {
-    const aElements = jQuery('li > a', '#readme');
+  const aElements = jQuery('li > a', '#readme');
 
-    const validElements = lodash.filter(aElements, (el) => {
-        const href = jQuery(el).attr('href');
-        const parsedHref = ParseGithubURL(href);
+  const validElements = lodash.filter(aElements, (el) => {
+    const href = jQuery(el).attr('href');
+    const parsedHref = ParseGithubURL(href);
 
-        if (parsedHref && parsedHref.host === 'github.com' && parsedHref.repo) {
-            return true;
-        }
-
-        return false;
-    });
-
-    async function elementIteratee(el) {
-        const href = jQuery(el).attr('href');
-        const { owner, name } = ParseGithubURL(href);
-        return appendStarTagAsync(el, owner, name);
+    if (parsedHref && parsedHref.host === 'github.com' && parsedHref.repo) {
+      return true;
     }
 
-    lodash.each(validElements, elementIteratee);
+    return false;
+  });
+
+  async function elementIteratee(el) {
+    const href = jQuery(el).attr('href');
+    const { owner, name } = ParseGithubURL(href);
+    return appendStarTagAsync(el, owner, name);
+  }
+
+  lodash.each(validElements, elementIteratee);
 }
 
 // Event Listeners //
 
 jQuery(document).ready(() => {
-    if (window.location.href.match(/awesome/i)) {
-        iterateAllLinks();
-    }
+  if (window.location.href.match(/awesome/i)) {
+    iterateAllLinks();
+  }
 });
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,20 +1,88 @@
-import $ from 'jquery';
+import jQuery from 'jquery';
 
-import {
-  fetchAccessTokenAsync,
-  fetchRateLimitAsync,
-  sendAccessTokenAsync,
-} from './utils/options';
+import anime from 'animejs';
+import { Client } from 'chomex';
+import numeral from 'numeral';
+
+import { TextColor } from './constants';
+
+const client = new Client(chrome.runtime);
+
+// Local Functions //
+
+function colorFromPercentage(percentage) {
+  switch (true) {
+    case (percentage < 2):
+      return TextColor.RED;
+    case (percentage >= 2 && percentage < 48):
+      return TextColor.ORANGE;
+    default:
+      return TextColor.GREEN;
+  }
+}
+
+// Exported Functions //
+
+function fetchAccessTokenAsync(Elem) {
+  return client
+    .message('/access-token/get')
+    .then(({ data }) => Elem.ACCESS_TOKEN_FIELD.val(data));
+}
+
+function fetchRateLimitAsync(Elem) {
+  return client
+    .message('/rate-limit')
+    .then((response) => {
+      const { remaining, limit } = response.data;
+      const formattedRateLimit = numeral(remaining).format('0,0');
+
+      if (limit === 0) {
+        Elem.ACCESS_TOKEN_INVALID.show();
+      }
+
+      let percentage = 0;
+      if (limit > 0) {
+        Elem.ACCESS_TOKEN_INVALID.hide();
+        percentage = parseInt((remaining / limit) * 100, 10);
+      }
+
+      const finalBGColor = colorFromPercentage(percentage);
+      const textStyle = { color: finalBGColor };
+      const $progressBarFilled = Elem.PROGRESS_BAR_FILLED;
+      $progressBarFilled.css({ width: '0%' });
+      anime({
+        targets: $progressBarFilled.get(0),
+        backgroundColor: finalBGColor,
+        easing: 'easeInOutQuad',
+        width: `${percentage}%`,
+        begin: () => Elem.PROGRESS_BAR_TEXT.css(textStyle).text(formattedRateLimit),
+      });
+    });
+}
+
+function sendAccessTokenAsync(Elem) {
+  const accessToken = Elem.ACCESS_TOKEN_FIELD.val();
+
+  return client.message('/access-token/set', { accessToken })
+    .then(() => {
+      const $accessTokenSaveButton = Elem.ACCESS_TOKEN_SAVE_BUTTON;
+      const origin = $accessTokenSaveButton.text();
+      $accessTokenSaveButton.text('saved!');
+      setTimeout(() => $accessTokenSaveButton.text(origin), 750);
+    })
+    .then(exports.fetchAccessTokenAsync(Elem))
+    .then(exports.fetchRateLimitAsync(Elem));
+}
 
 // Event Listeners //
 
-$(document).ready(() => {
+jQuery(document).ready(() => {
   const Elem = {
-    ACCESS_TOKEN_FIELD: $('#access-token-field'),
-    ACCESS_TOKEN_INVALID: $('.access-token-invalid'),
-    ACCESS_TOKEN_SAVE_BUTTON: $('#access-token-save-button'),
-    PROGRESS_BAR_FILLED: $('.progress-bar-filled'),
-    PROGRESS_BAR_TEXT: $('.progress-bar-text'),
+    ACCESS_TOKEN_FIELD: jQuery('#access-token-field'),
+    ACCESS_TOKEN_INVALID: jQuery('.access-token-invalid'),
+    ACCESS_TOKEN_SAVE_BUTTON: jQuery('#access-token-save-button'),
+    PROGRESS_BAR_FILLED: jQuery('.progress-bar-filled'),
+    PROGRESS_BAR_TEXT: jQuery('.progress-bar-text'),
   };
 
   Elem.ACCESS_TOKEN_INVALID.hide();
