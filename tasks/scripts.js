ba39b3f72c846a5fed30ae01c20e4696f2e9457a
Heng-Yi Wu <henry40408@gmail.com>
2017-06-25 20:59:06 +0800

    chore(gulp): reformat scripts task

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -10,50 +10,32 @@ import args from './lib/args';
 
 const ENV = args.production ? 'production' : 'development';
 
-gulp.task('scripts', (cb) => {
-  return gulp.src('app/scripts/*.js')
-    .pipe(plumber({
-      errorHandler: function() {
-        // Webpack will log the errors
-      }
-    }))
-    .pipe(named())
-    .pipe(gulpWebpack({
-      devtool: args.sourcemaps ? 'inline-source-map' : false,
-      watch: args.watch,
-      plugins: [
-        new webpack.DefinePlugin({
-          'process.env': {
-            'NODE_ENV': JSON.stringify(ENV)
-          },
-          '__ENV__': JSON.stringify(ENV),
-          '__VENDOR__': JSON.stringify(args.vendor)
-        }),
-      ].concat(args.production ? [
-        new webpack.optimize.UglifyJsPlugin()
-      ] : []),
-      module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'eslint-loader',
-            exclude: /node_modules/,
-            enforce: 'pre'
-          },
-          {
-            test: /\.js$/,
-            loader: 'babel-loader',
-            enforce: 'post'
-          }
-        ]
-      }
-    }, webpack, (err, stats) => {
-      log(`Finished '${colors.cyan('scripts')}'`, stats.toString({
-        chunks: false,
-        colors: true,
-        cached: false,
-        children: false
-      }));
-    }))
-    .pipe(gulp.dest(`dist/${args.vendor}/scripts`))
-    .pipe(gulpif(args.watch, livereload()));
-});
+gulp.task('scripts', () => gulp.src('app/scripts/*.js')
+  .pipe(plumber({ errorHandler: function() { /* Webpack will log the errors */ } }))
+  .pipe(named())
+  .pipe(gulpWebpack({
+    devtool: args.sourcemaps ? 'inline-source-map' : false,
+    watch: args.watch,
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': { NODE_ENV: JSON.stringify(ENV) },
+        __ENV__: JSON.stringify(ENV),
+        __VENDOR__: JSON.stringify(args.vendor),
+      }),
+    ].concat(args.production ? [new webpack.optimize.UglifyJsPlugin()] : []),
+    module: {
+      rules: [
+        { test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/, enforce: 'pre' },
+        { test: /\.js$/, loader: 'babel-loader', enforce: 'post' }
+      ],
+    },
+  }, webpack, (err, stats) => {
+    log(`Finished '${colors.cyan('scripts')}'`, stats.toString({
+      chunks: false,
+      colors: true,
+      cached: false,
+      children: false,
+    }));
+  }))
+  .pipe(gulp.dest(`dist/${args.vendor}/scripts`))
+  .pipe(gulpif(args.watch, livereload())));
