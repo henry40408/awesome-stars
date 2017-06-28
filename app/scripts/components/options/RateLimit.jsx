c37fddfa7a4c087c0b33ad0b099c408227578f8a
Heng-Yi Wu <henry40408@gmail.com>
2017-06-28 10:44:46 +0800

    refactor(options): rate limit progress bar animation

--- app/scripts/components/options/RateLimit.jsx
+++ app/scripts/components/options/RateLimit.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Flex } from 'reflexbox';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { SText } from '../common';
 import COLORS from '../../services/colors';
@@ -15,8 +15,34 @@ const SProgressBar = styled.div`
   height: ${rem(48)};
 `;
 
-const SProgressBarContainer = styled(Flex) `
+SProgressBar.Container = styled(Flex) `
   margin: 0 0 ${rem(8)};
+`;
+
+const colorFromPercentage = (percentage) => {
+  if (percentage >= 50) {
+    return COLORS.GREEN;
+  } else if (percentage > 2 && percentage < 50) {
+    return COLORS.YELLOW;
+  }
+  return COLORS.RED;
+};
+
+const filling = props => keyframes`
+  from {
+    background-color: ${COLORS.RED};
+    width: 0%;
+  }
+
+  to {
+    background-color: ${colorFromPercentage(props.percentage)};
+    width: ${props.percentage}%;
+  }
+`;
+
+SProgressBar.Fill = styled.div`
+  animation: 1.5s ease 1s 1 normal forwards running ${props => filling(props)};
+  height: 100%;
 `;
 
 const SRateLimit = styled.div`
@@ -29,10 +55,14 @@ const SNumber = styled(Box) `
 
 const RateLimit = () => (
   <SRateLimit>
-    <SProgressBarContainer>
-      <Box w={4 / 5}><SProgressBar /></Box>
+    <SProgressBar.Container>
+      <Box w={4 / 5}>
+        <SProgressBar>
+          <SProgressBar.Fill percentage={0} />
+        </SProgressBar>
+      </Box>
       <SNumber flex w={1 / 5} align="center" justify="center">{'N/A'}</SNumber>
-    </SProgressBarContainer>
+    </SProgressBar.Container>
     <SHelp>{'For requests using Basic Authentication or OAuth (including access token), you can make up to 5,000 requests per hour.'}</SHelp>
   </SRateLimit>
 );
