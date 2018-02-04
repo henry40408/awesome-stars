78de14785ddbf3fb7def86b6c819a5ff14ff483a
Heng-Yi Wu <henry40408@gmail.com>
2018-02-04 17:28:11 +0800

    refactor: Replace SCSS with injectGlobal

--- app/scripts/options.jsx
+++ app/scripts/options.jsx
@@ -1,4 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'styled-components';
+
+import colors from './themes/colors';
+
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+html,
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 16px;
+}
+
+body {
+  background-image: url('../images/options-background.svg');
+  color: ${colors.white};
+}
+
+a {
+  color: ${colors.orange};
+}
+`;
 
 ReactDOM.render(<div />, document.getElementById('container'));
