ff16a541a836aa61a3d870ac2ea62303caea47e6
Heng-Yi Wu <henry40408@gmail.com>
2018-02-06 00:43:12 +0800

    chore: Introduce webpack-bundle-analyzer

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -6,37 +6,58 @@ import webpack from 'webpack';
 import gulpWebpack from 'webpack-stream';
 import plumber from 'gulp-plumber';
 import livereload from 'gulp-livereload';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
 import args from './lib/args';
 
 const ENV = args.production ? 'production' : 'development';
 
-gulp.task('scripts', () => gulp.src('app/scripts/*.+(js|jsx)')
-  .pipe(plumber({ errorHandler: function() { /* Webpack will log the errors */ } }))
-  .pipe(named())
-  .pipe(gulpWebpack({
-    devtool: args.sourcemaps ? 'inline-source-map' : false,
-    watch: args.watch,
-    resolve: { extensions: ['.js', '.jsx'] },
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env': { NODE_ENV: JSON.stringify(ENV) },
-        __ENV__: JSON.stringify(ENV),
-        __VENDOR__: JSON.stringify(args.vendor),
+gulp.task('scripts', () =>
+  gulp
+    .src('app/scripts/*.+(js|jsx)')
+    .pipe(
+      plumber({
+        errorHandler: function() {
+          /* Webpack will log the errors */
+        },
       }),
-    ].concat(args.production ? [new webpack.optimize.UglifyJsPlugin()] : []),
-    module: {
-      rules: [
-        { test: /\.jsx?$/, loader: 'eslint-loader', exclude: /node_modules/, enforce: 'pre' },
-        { test: /\.jsx?$/, loader: 'babel-loader', enforce: 'post' }
-      ],
-    },
-  }, webpack, (err, stats) => {
-    log(`Finished '${colors.cyan('scripts')}'`, stats.toString({
-      chunks: false,
-      colors: true,
-      cached: false,
-      children: false,
-    }));
-  }))
-  .pipe(gulp.dest(`dist/${args.vendor}/scripts`))
-  .pipe(gulpif(args.watch, livereload())));
+    )
+    .pipe(named())
+    .pipe(
+      gulpWebpack(
+        {
+          devtool: args.sourcemaps ? 'inline-source-map' : false,
+          watch: args.watch,
+          resolve: { extensions: ['.js', '.jsx'] },
+          plugins: [
+            new webpack.DefinePlugin({
+              'process.env': { NODE_ENV: JSON.stringify(ENV) },
+              __ENV__: JSON.stringify(ENV),
+              __VENDOR__: JSON.stringify(args.vendor),
+            }),
+            new BundleAnalyzerPlugin(),
+          ].concat(args.production ? [new webpack.optimize.UglifyJsPlugin()] : []),
+          module: {
+            rules: [
+              { test: /\.jsx?$/, loader: 'eslint-loader', exclude: /node_modules/, enforce: 'pre' },
+              { test: /\.jsx?$/, loader: 'babel-loader', enforce: 'post' },
+            ],
+          },
+        },
+        webpack,
+        (err, stats) => {
+          log(
+            `Finished '${colors.cyan('scripts')}'`,
+            stats.toString({
+              chunks: false,
+              colors: true,
+              cached: false,
+              children: false,
+            }),
+          );
+        },
+      ),
+    )
+    .pipe(gulp.dest(`dist/${args.vendor}/scripts`))
+    .pipe(gulpif(args.watch, livereload())),
+);
