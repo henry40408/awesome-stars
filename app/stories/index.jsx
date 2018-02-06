7d150a22283e2b37ca4f76cfcc0a13613c5bd4ec
Heng-Yi Wu <henry40408@gmail.com>
2018-02-07 01:52:03 +0800

    chore: Introduce knobs addon of storybook

--- app/stories/index.jsx
+++ app/stories/index.jsx
@@ -1,55 +1,41 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, boolean, number, text } from '@storybook/addon-knobs';
+
+import styled from 'styled-components';
 
 import AccessTokenForm from '../scripts/components/AccessTokenForm';
 import RateLimit from '../scripts/components/RateLimit';
 
 const MAXIMUM = 5000;
 
-storiesOf('RateLimit', module)
-  .add('default', () => <RateLimit />)
-  .add('inverse', () => <RateLimit inverse remaining={MAXIMUM} total={MAXIMUM} />)
-  .add('with remaining and total', () => (
-    <div>
-      <p>empty</p>
-      <RateLimit remaining={0} total={MAXIMUM} />
-      <p>1.9%</p>
-      <RateLimit remaining={MAXIMUM * 0.019} total={MAXIMUM} />
-      <p>49.9%</p>
-      <RateLimit remaining={MAXIMUM * 0.499} total={MAXIMUM} />
-      <p>total</p>
-      <RateLimit remaining={MAXIMUM} total={MAXIMUM} />
-    </div>
-  ))
-  .add('with heightInRem', () => (
-    <div>
-      <p>1</p>
-      <RateLimit remaining={MAXIMUM} total={MAXIMUM} heightInRem={1} />
-      <p>2</p>
-      <RateLimit remaining={MAXIMUM} total={MAXIMUM} heightInRem={2} />
-      <p>4</p>
-      <RateLimit remaining={MAXIMUM} total={MAXIMUM} heightInRem={4} />
-    </div>
-  ));
+addDecorator(withKnobs);
 
-const onAccessTokenSubmit = accessToken => action(`access token submitted: ${accessToken}`);
+const RateLimitContainer = styled.div`
+  background-color: ${({ inverse }) => (inverse ? 'black' : 'white')};
+  padding: 1rem 0;
+`;
 
-storiesOf('AccessTokenForm', module)
-  .add('default', () => <AccessTokenForm />)
-  .add('with height', () => (
-    <div>
-      <p>1rem</p>
-      <AccessTokenForm heightInRem={1} />
-      <p>2rem</p>
-      <AccessTokenForm heightInRem={2} />
-      <p>4rem</p>
-      <AccessTokenForm heightInRem={4} />
-    </div>
-  ))
-  .add('with onSubmit handler', () => (
-    <AccessTokenForm heightInRem={2} onSubmit={onAccessTokenSubmit} />
-  ))
-  .add('with accessToken', () => (
-    <AccessTokenForm heightInRem={2} accessToken="accessToken" onSubmit={onAccessTokenSubmit} />
-  ));
+storiesOf('RateLimit', module).add('default', () => {
+  const inverse = boolean('Inverse', false);
+
+  return (
+    <RateLimitContainer inverse={inverse}>
+      <RateLimit
+        inverse={inverse}
+        remaining={number('Remaining', MAXIMUM)}
+        total={number('Total', MAXIMUM)}
+        heightInRem={number('Height in rem', 1)}
+      />
+    </RateLimitContainer>
+  );
+});
+
+storiesOf('AccessTokenForm', module).add('default', () => (
+  <AccessTokenForm
+    accessToken="accessToken"
+    heightInRem={number('Height in rem', 1)}
+    onSubmit={accessToken => action(`access token submitted: ${accessToken}`)}
+  />
+));
